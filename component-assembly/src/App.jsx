f0090e62f712be16e62757859c2de7ea05192f3b
seanwillex <seanwillex@gmail.com>
2024-11-06 21:03:53 +0100

    Update App.jsx

--- component-assembly/src/App.jsx
+++ component-assembly/src/App.jsx
@@ -1,9 +1,13 @@
 import BannerSection from './components/BannerSection'
+import PlanTripSection from './components/PlanTripSection'
 
 function App() {
   return (
     <div className="min-h-screen bg-[#0a0a12]">
       <BannerSection />
+      <div className="py-20">
+        <PlanTripSection />
+      </div>
     </div>
   )
 }
