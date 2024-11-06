e9675576e37d88caca845a11f52eafc71821b060
seanwillex <seanwillex@gmail.com>
2024-11-06 01:12:00 +0100

    Create App.jsx

---
+++ component-assembly/src/App.jsx
@@ -0,0 +1,11 @@
+import BannerSection from './components/BannerSection'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#0a0a12]">
+      <BannerSection />
+    </div>
+  )
+}
+
+export default App
